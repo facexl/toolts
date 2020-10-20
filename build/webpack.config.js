287a72503f5417cea96263cf4c2ffe30c75ccdb1
xiaolang <facelanglang@gmail.com>
2020-10-20 14:56:59 +0800

    config

---
+++ build/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    entry:'./src/index.ts',
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+          // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+          { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    }
+}
